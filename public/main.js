298259ca5e232a92c2eed6b6ddb0a19be80f234b
Nolan Jacobson <theprophet0@users.noreply.github.com>
2019-11-06 17:36:36 -0500

    explorerMode

--- public/main.js
+++ public/main.js
@@ -1,7 +1,30 @@
+const deck = []
+const suits = ['Spades', 'Diamonds', 'Clubs', 'Hearts']
+const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+let j = 0
+let card
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  for (let i = 0; i < suits.length; i++) {
+    for (let m = 0; m < values.length; m++) {
+      card = values[m] + ' of ' + suits[i]
+      deck.push(card)
+    }
+  }
+  console.log(deck)
+
+  for (i = 52; i >= 1; i--) {
+    j = Math.floor(Math.random() * i)
+
+    const swapper = deck[j]
+    deck[j] = deck[i]
+    deck[i] = swapper
   }
 }
 
+const clickTheDeck = () => {
+  document.querySelector('.cardResult').textContent = deck.pop()
+}
+
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.deck').addEventListener('click', clickTheDeck)
